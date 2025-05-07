85640e9dac8188990a71a57983380c28a57860b9
Abdulaziz Ibiyeye <114391255+ife4lv@users.noreply.github.com>
2025-05-07 00:06:36 -0500

    chore: declare vpc and sg modules in main.tf and add missing variables

--- main.tf
+++ main.tf
@@ -1,9 +1,37 @@
+// main.tf
+
+// 1. Provider (we moved this to provider.tf)
+// 2. VPC Module
+module "vpc" {
+  source              = "./modules/vpc"
+  name                = "techchall1"
+  cidr_block          = var.vpc_cidr_block
+  public_subnet_cidrs = var.public_subnet_cidrs
+}
+
+// 3. Security Group Module (we’ll refer to it as "sg")
+module "sg" {
+  source   = "./modules/security_group"
+  name     = "techchall1"
+  vpc_id   = module.vpc.vpc_id
+}
+
+// 4. Jenkins EC2 Module
 module "jenkins" {
   source            = "./modules/jenkins_ec2"
   name              = "techchall1"
-  ami_id            = "ami-04f167a56786e4b09"
-  instance_type     = "t2.micro"
-  key_name          = "W7TC"
+  ami_id            = var.ami_id            // or hardcode your AMI if preferred
+  instance_type     = "t3.micro"
+  key_name          = var.key_name
   subnet_id         = module.vpc.public_subnet_ids[0]
   security_group_id = module.sg.jenkins_sg_id
+}
+
+// 5. ECS Cluster Module
+module "ecs" {
+  source             = "./modules/ecs_cluster"
+  name               = "techchall1"
+  vpc_id             = module.vpc.vpc_id
+  subnet_ids         = module.vpc.public_subnet_ids
+  security_group_ids = [module.sg.ecs_sg_id]
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,27 @@ variable "aws_region" {
   type        = string
   default     = "us-east-2"
 }
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDRs for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "ami_id" {
+  description = "AMI ID for Jenkins EC2"
+  type        = string
+  default     = "ami-04f167a56786e4b09"  // your Ubuntu AMI
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+  default     = "W7TC"
+}
