9129c39a2bc6aa90cb77f0186529fda841a108da
Abdulaziz Ibiyeye <114391255+ife4lv@users.noreply.github.com>
2025-05-07 23:14:06 -0500

    Update on output.tf, and both frontend and backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,5 @@
-export const API_URL = 'http://localhost:8080/'
-export default API_URL
+// export const API_URL = 'http://localhost:8080/'
+// export default API_URL
+
+const API_BASE_URL = "http://host.docker.internal:8080";
+export default API_BASE_URL;
